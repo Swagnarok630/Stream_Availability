1dccc90b7b01f5b924a713f39b1c97d578a2e9d6
Swagnarok <darryl.le.630@gmail.com>
2022-04-30 12:51:15 -0700

    deleted some comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-// Logic for page
-// api: 0ec05b3931msh88228e405c88947p14f250jsn5fa8105f9f8a
-
 // Utility Functions
 var queryAll = element => document.querySelectorAll(element); // Returns a nodeList
 var querySel = element => document.querySelector(element); // Returns a node
