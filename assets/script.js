4bea427d80ea2dc6721e89e12b9d83774aa86955
Tonyavall <tonyavallescas@gmail.com>
2022-04-30 12:02:11 -0700

    .fetch API addons from class

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+// Documents
+// genreBtns = document.
+
 // Utility Functions
 var queryAll = element => document.querySelectorAll(element); // Returns a nodeList
 var querySel = element => document.querySelector(element); // Returns a node
@@ -10,7 +13,7 @@ var getId = element => document.getElementById(element) // Returns an HTML eleme
 var index = 1;
 
 // Replace 'element here' with the html parents that have/will have class hidden
-var forListeners = queryAll('element here')
+var forListeners = queryAll('#landing-page')
 
 // Loops over all nodes and returns the node that the user is currently on
 function currentIndex() {
@@ -32,18 +35,25 @@ function showCurrent() {
     currentIndex().removeAttribute("class");
 };
 
-function getApi() {
-    var requestUrl = '';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
+		'X-RapidAPI-Key': '0a6c780725msh1dabbdd8d99ac58p1adc10jsna65e0cb9d583'
+	}
+};
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            for (var i = 0; data.length; i++) {
-                console.log(data[i].length)
-            }
-        })
-}
+fetch('https://streaming-availability.p.rapidapi.com/genres', options)
+	.then(response => response.json())
+	.then((response) => {
+        var genres = Object.values(response)
+        // Check to see that genres is returned
+        // console.log(genres)
 
-getApi()
+        // fo
+        var newBtn = document.createElement('button');
+        var btnContent = document.textContent(genres)
+        newBtn.appendChild(btnContent)
+
+    })
+	.catch(err => console.error(err));
