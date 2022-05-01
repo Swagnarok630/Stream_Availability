fc59a3f41d5afcc5e81d0b8d32bd8c6bbd5af131
Tonyavall <tonyavallescas@gmail.com>
2022-04-30 19:58:21 -0700

    Added userRequest and randomizePage function. Also added a partial key loop authentication. None of the keys work right now, so the functionality is unconfirmed.

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,20 @@ var getClass = element => document.getElementsByClassName(element)
 
 // Global Variables
 var index = 1;
+var randomizedPageNumber = [];
 
 userInput = {
-    services: [],
-    genre: [],
-    type: [],
+    services: ['netflix'],
+    genre: ['4'],
+    type: ['movie'],
 }
+
+// API KEYS
+var chase = '0ec05b3931msh88228e405c88947p14f250jsn5fa8105f9f8a'
+var tony = '0a6c780725msh1dabbdd8d99ac58p1adc10jsna65e0cb9d583'
+var darryl = 'b1772a2b66msh8fe7f52298f657ep156885jsn20747bc84c72'
+
+var apiKeys = [chase, tony, darryl]
 
 // Replace 'element here' with the html parents that have/will have class hidden
 var forListeners = queryAll('#landing-page')
@@ -39,14 +47,76 @@ function showCurrent() {
     currentIndex().removeAttribute("class");
 };
 
+function randomizePage() {
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
+            'X-RapidAPI-Key': ''
+        }
+    };
 
-// netflix + hulu + kids + movie -> array
+    var services = userInput.services.join('%2C%20')
+    var genre = userInput.genre.join('%2C%20')
+    var type = userInput.type.join('%2C%20')
 
-let services = userInput.services.join('%2C%20')
-let genre = userInput.genre.join('%2C%20')
-let type = userInput.type.join('%2C%20')
+    fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=' + services + '&type=' + type + '&genre=' + genre + '&output_language=en&language=en', options)
+        .then(response => {
+            // Looping through apiKeys
+            for (var i = 0; i < apiKeys.length; i++) {
+                // If the current API key is equal to the key we already have, continue aka skip current key
+                if (apiKeys[i] === options.headers["X-RapidAPI-Key"]) {
+                    continue;
+                // If the response comes out invalid
+                } else if (!response.ok) {
+                    // Set the current iteration key equal to the options API key
+                    options.headers["X-RapidAPI-Key"] = apiKeys[i];
+                    // Fetch with current key
+                    fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=' + services + '&type=' + type + '&genre=' + genre + '&output_language=en&language=en', options)
+                } else {
+                    // Once the fetch request is valid, we will json that data
+                    response.json()
+                    // We will also break the loop so it doesn't keep trying to loop until apiKeys.length
+                    break;
+                }
+            }
+        })
+        .then((response) => {
+            randomizedPageNumber.push(Math.floor((Math.random() * response.total_pages) + 1))
+        })
+        .catch(err => console.error(err));
+}
 
-fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=' + services + '&type=' + type + '&genre=18&output_language=en&language=en')
+randomizePage();
+
+function userRequest() {
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
+            'X-RapidAPI-Key': darryl
+        }
+    };
+
+    var services = userInput.services.join('%2C%20')
+    var genre = userInput.genre.join('%2C%20')
+    var type = userInput.type.join('%2C%20')
+
+    console.log(randomizedPageNumber)
+    fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=' + services + '&type=' + type + '&genre=' + genre + '&page=' + randomizedPageNumber + '&output_language=en&language=en', options)
+        .then(response => response.json())
+        
+        .then((response) => {
+            // Statements with what to do with data pulled by the user
+            console.log(response)
+        })
+        .catch(err => console.error(err));
+}
+
+setTimeout( () => {
+    userRequest();
+} , 5000
+);
 
 
 // Unused Code
